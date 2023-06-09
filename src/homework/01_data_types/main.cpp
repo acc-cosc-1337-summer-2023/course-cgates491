bad8f69acf56a79a84c5541d6fa336e508c29dfe
cgates491 <135079985+cgates491@users.noreply.github.com>
2023-06-09 20:59:53 +0000

    homework1

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,24 @@
 ﻿//write include statements
-
-
+#include<iostream>
+#include "data_types.h"
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 int main()
 {
-	
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
+
+	int result;
+	result = multiply_numbers(num);
+
+	cout<<"your number is " <<result;
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout<<" "<<result;
 
 	return 0;
 }
